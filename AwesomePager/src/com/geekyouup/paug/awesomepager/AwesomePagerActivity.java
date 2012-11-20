ee55d43d889b275ee5d50c301f0b8264974a1fcb
richard.hyndman <richard.hyndman@gmail.com>
2012-11-20 11:44:17 +0000

    Updated deprecated methods and removed unnecessary casts

--- AwesomePager/src/com/geekyouup/paug/awesomepager/AwesomePagerActivity.java
+++ AwesomePager/src/com/geekyouup/paug/awesomepager/AwesomePagerActivity.java
@@ -8,6 +8,7 @@ import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class AwesomePagerActivity extends Activity {
@@ -41,21 +42,21 @@ public class AwesomePagerActivity extends Activity {
 	     * Create the page for the given position.  The adapter is responsible
 	     * for adding the view to the container given here, although it only
 	     * must ensure this is done by the time it returns from
-	     * {@link #finishUpdate()}.
+	     * {@link #finishUpdate(android.view.ViewGroup)}.
 	     *
-	     * @param container The containing View in which the page will be shown.
+	     * @param collection The containing View in which the page will be shown.
 	     * @param position The page position to be instantiated.
 	     * @return Returns an Object representing the new page.  This does not
 	     * need to be a View, but can be some other container of the page.
 	     */
 		@Override
-		public Object instantiateItem(View collection, int position) {
+		public Object instantiateItem(ViewGroup collection, int position) {
 			TextView tv = new TextView(cxt);
-			tv.setText("Bonjour PAUG " + position);
+			tv.setText(getString(R.string.app_message) + position);
 			tv.setTextColor(Color.WHITE);
 			tv.setTextSize(30);
 			
-			((ViewPager) collection).addView(tv,0);
+			collection.addView(tv,0);
 			
 			return tv;
 		}
@@ -63,23 +64,30 @@ public class AwesomePagerActivity extends Activity {
 	    /**
 	     * Remove a page for the given position.  The adapter is responsible
 	     * for removing the view from its container, although it only must ensure
-	     * this is done by the time it returns from {@link #finishUpdate()}.
+	     * this is done by the time it returns from {@link #finishUpdate(android.view.ViewGroup)}.
 	     *
-	     * @param container The containing View from which the page will be removed.
+	     * @param collection The containing View from which the page will be removed.
 	     * @param position The page position to be removed.
-	     * @param object The same object that was returned by
-	     * {@link #instantiateItem(View, int)}.
+	     * @param view The same object that was returned by
+	     * {@link #instantiateItem(android.view.View, int)}.
 	     */
 		@Override
-		public void destroyItem(View collection, int position, Object view) {
-			((ViewPager) collection).removeView((TextView) view);
+		public void destroyItem(ViewGroup collection, int position, Object view) {
+			collection.removeView((TextView) view);
 		}
 
-		
-		
+
+        /**
+         * Determines whether a page View is associated with a specific key object
+         * as returned by instantiateItem(ViewGroup, int). This method is required
+         * for a PagerAdapter to function properly.
+         * @param view Page View to check for association with object
+         * @param object Object to check for association with view
+         * @return
+         */
 		@Override
 		public boolean isViewFromObject(View view, Object object) {
-			return view==((TextView)object);
+			return (view==object);
 		}
 
 		
@@ -87,11 +95,11 @@ public class AwesomePagerActivity extends Activity {
 	     * Called when the a change in the shown pages has been completed.  At this
 	     * point you must ensure that all of the pages have actually been added or
 	     * removed from the container as appropriate.
-	     * @param container The containing View which is displaying this adapter's
+	     * @param arg0 The containing View which is displaying this adapter's
 	     * page views.
 	     */
 		@Override
-		public void finishUpdate(View arg0) {}
+		public void finishUpdate(ViewGroup arg0) {}
 		
 
 		@Override
@@ -103,7 +111,7 @@ public class AwesomePagerActivity extends Activity {
 		}
 
 		@Override
-		public void startUpdate(View arg0) {}
+		public void startUpdate(ViewGroup arg0) {}
     	
     }
     
